97c6c7050d2191bbce1c359ee75d27b160ba70bf
SomyaGautam02 <somyagautam02@gmail.com>
2023-07-04 22:29:24 +0530

    minor changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className='footer'>
         <div className="footer__heading">
-            Made with love 💙 by<a href='#a'> &nbsp; Somya Gautam</a> 
+            Made with love 💙 by<a href='https://somyagautam.pages.dev/'> &nbsp; Somya Gautam</a> 
         </div>
         <div className="footer__heading"> &#169; SomyaGautam. All Rights Reserved</div>
     </div>
